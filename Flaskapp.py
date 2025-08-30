8f92ceba03083eb7e93e4c435eca18875490e1ce
Joshua Mark Castillo <joshuamarkcastillo0319@gmail.com>
2025-08-30 17:04:18 +0800

    Changed Flaskapp to a working to-do app, Created Html landing page

--- Flaskapp.py
+++ Flaskapp.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+tasks = ["Learn Flask", "Build To-Do App", "Push to GitHub"]
+
 @app.route("/")
 def home():
-    return "Hello, Flask World!(inside venv)"
+    return render_template("index.html", tasks=tasks)
 
-@app.route("/about")
-def about():
-    return "This is the About page (inside venv)."
+# This function is connected to the /add forms in index
+@app.route("/add", methods=["POST"])
+def add_task():
+    task = request.form.get("task") # get the input from the form
+    if task:
+        tasks.append(task) # add to the list
+    return redirect(url_for("home")) # redirect to home
 
 if __name__ == "__main__":
     app.run(debug=True)
